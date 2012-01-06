dcee27807c16f9b146fe8141c259daa06bd3c072
Tom Deweerdt <tom.deweerdt@capgemini.com>
2012-01-06 10:31:53 +0100

    constants added

---
+++ FindClusters/src/structurer/Constants.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package structurer;
+
+/**
+ * @author TDEWEERD
+ *
+ */
+public class Constants {
+
+	static final String TABLE2PROGRAM_XREF = "/resources/xref_table_program.csv";
+	static final String TABLE2MODULE_XREF = "/resources/xref_table_module.csv";
+	static final String PROGFILE = "/resources/ifs_programs.csv";
+	//	D:\DVANDECA\Desktop\My Documents\My LabsWork\GitRepositories\Clustering\FindClusters\src\resources\ifs_programs.csv
+
+}
